0cf532fd314f8696cbb5d7e7f9c81b268606e10f
kiryteo <capnoi8@gmail.com>
2018-06-17 20:23:25 +0530

    Added 'generator.py' to generate db_source and added routine for invoking this file in 'root-pkg'

---
+++ resolver/generator.py
@@ -0,0 +1,36 @@
+import os
+import re
+
+cwd = os.getcwd()
+
+rootdir = cwd + "/build" 
+
+#os.environ['ROOTSOURCES']
+
+rule1 = re.compile('.*name:.*')
+rule2 = re.compile('.*deps:.*')
+rule3 = re.compile('.*path:.*')
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith(".yml"):
+            fp = os.path.join(subdir, file)
+            with open(fp) as t:
+                fl = t.read()
+                r1 = rule1.findall(fl)
+                r1 = [x.strip(' name: ') for x in r1 ]
+                r2 = rule2.findall(fl)
+                r3 = rule3.findall(fl)
+                if r1:
+                    fl2 = open("manifest.yml", 'a')
+                    fl2.write(",".join(r1))
+                    fl2.write(":")
+                    fl2.write("\n")
+                if r1:
+                    fl2 = open("manifest.yml", 'a')
+                    fl2.write(",".join(r2))
+                    fl2.write("\n")
+                if r1:
+                    fl2 = open("manifest.yml", 'a')
+                    fl2.write(",".join(r3))
+                    fl2.write("\n")
